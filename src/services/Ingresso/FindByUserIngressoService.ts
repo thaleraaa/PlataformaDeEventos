e9b64b393356aff7a63d7909b1e44aea7b336e70
Thales <d2024001956@unifei.edu.br>
2026-05-19 11:35:15 -0300

    feat: implementa services de ingresso

---
+++ src/services/Ingresso/FindByUserIngressoService.ts
@@ -0,0 +1,17 @@
+import prismaClient from "../../prisma";
+
+class FindByUserIngressoService {
+    async execute (user_id: string) {
+        if(!user_id) {
+            throw new Error ("Dados incompletos");
+        }
+
+        return prismaClient.ingresso.findMany({
+            where: {
+                user_id: user_id
+            }
+        });
+    }
+}
+
+export { FindByUserIngressoService }
