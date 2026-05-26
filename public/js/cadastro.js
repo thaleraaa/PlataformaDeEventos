8d290b66e7f9135699e4460a1ac433ce11edd64e
Thales <d2024001956@unifei.edu.br>
2026-05-26 14:40:48 -0300

    feat: implementa integração do cadastro

---
+++ public/js/cadastro.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('#form-cadastro');
+const nomeINPUT = document.querySelector('#nome');
+const emailINPUT = document.querySelector('#email');
+const senhaINPUT = document.querySelector('#senha');
+
+
+const URL_API = 'http://localhost:3333/v1/user';
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const nome = nomeINPUT.value;
+    const email = emailINPUT.value;
+    const senha = senhaINPUT.value;
+
+    const response = await fetch(URL_API, {
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            nome: nome,
+            email: email,
+            senha: senha
+        })
+    });
+
+    if(!response.ok) {
+        alert('Não foi possivel criar o usuario!');
+        return;
+    }
+
+    window.location.href = 'http://127.0.0.1:5500/public/pages/login.html'
+    alert('Cadastro feito com sucesso!');
+});
