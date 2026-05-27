05c7f4d6a8d659137ae76436fb57d9a9e96a7b50
Thales <d2024001956@unifei.edu.br>
2026-05-27 19:12:44 -0300

    feat: implementa exibição de CEP

--- src/services/Eventos/CreateEventoService.ts
+++ src/services/Eventos/CreateEventoService.ts
@@ -2,8 +2,8 @@ import prismaClient from "../../prisma";
 import { CreateEventoRequest } from "../../models/interfaces/Eventos/CreateEventoRequest";
 
 class CreateEventoService {
-    async execute ({nome, data, horario, valor, user_id, imagem} : CreateEventoRequest) {
-        if(!nome || !data || !horario || !valor || !user_id) {
+    async execute ({nome, data, horario, valor, user_id, imagem, rua, bairro, cidade, estado} : CreateEventoRequest) {
+        if(!nome || !data || !horario || !valor || !user_id || !rua || !bairro || !cidade || !estado) {
             throw new Error ("Dados incompletos");
         }
 
@@ -16,7 +16,11 @@ class CreateEventoService {
                 horario: horario,
                 valor: valorNumber,
                 user_id: user_id,
-                imagem: imagem
+                imagem: imagem,
+                rua: rua,
+                bairro: bairro,
+                cidade: cidade,
+                estado: estado
             }
         });
 
